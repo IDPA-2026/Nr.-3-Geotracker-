a57a2430aefd8f23331065434f95dca7b522ff72
Rafafi35 <salinger.rafael@gmail.com>
2026-03-07 22:00:31 +0100

    add Tracker Location on Map

--- web-frontend/app/map.tsx
+++ web-frontend/app/map.tsx
@@ -1,9 +1,17 @@
 "use client"
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import L from "leaflet"
 import "leaflet/dist/leaflet.css";
 
 export default function map(){
+
+    const location = L.icon({
+        iconUrl: "/location.svg",
+        iconSize: [32, 32],
+        iconAnchor: [16, 16],
+    })
+
     return(
         <MapContainer
             center={[47.3769, 8.5417]}
@@ -14,6 +22,9 @@ export default function map(){
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            <Marker position={[47.3769, 8.5417]} icon={location}/>
+
         </MapContainer>
     )
 }
